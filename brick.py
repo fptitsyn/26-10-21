f78518f6089c956b4b87b48448f497db8cbf74f0
fptitsyn <fedya.ptitsyn@mail.ru>
2021-10-27 18:36:43 +0300

    Added check for a != 0

--- brick.py
+++ brick.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-a = float(input("a: "))
-b = float(input("b: "))
-c = float(input("c: "))
-x = float(input("x: "))
-y = float(input("y: "))
+if __name__ == "__main__":
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = float(input("c: "))
+    x = float(input("x: "))
+    y = float(input("y: "))
 
-s1 = a * c
-s2 = x * y
+    s1 = a * c
+    s2 = x * y
 
-if s1 < s2:
-    print("Кирпич пройдёт")
-else:
-    print("Кирпич не пройдёт")
+    if s1 < s2:
+        print("Кирпич пройдёт")
+    else:
+        print("Кирпич не пройдёт")
--- square_eq.py
+++ square_eq.py
@@ -3,18 +3,22 @@
 
 from math import sqrt
 
-a = float(input("a: "))
-b = float(input("b: "))
-c = float(input("c: "))
+if __name__ == "__main__":
+    a = float(input("a: "))
+    if a != 0:
+        b = float(input("b: "))
+        c = float(input("c: "))
 
-D = b * b - 4 * a * c
+        D = b * b - 4 * a * c
 
-if D > 0:
-    x1 = (-b - sqrt(D)) / (2 * a)
-    x2 = (-b + sqrt(D)) / (2 * a)
-    print(x1, x2)
-elif D == 0:
-    x = -b / (2 * a)
-    print(x)
-else:
-    print("No roots.")
+        if D > 0:
+            x1 = (-b - sqrt(D)) / (2 * a)
+            x2 = (-b + sqrt(D)) / (2 * a)
+            print(x1, x2)
+        elif D == 0:
+            x = -b / (2 * a)
+            print(x)
+        else:
+            print("No roots.")
+    else:
+        print("a can't be zero.")
